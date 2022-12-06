80dced8d0eb2fb6b1c5cb9c4f24a2416fdef9bb3
Theivendram Athavan <thavanthya@gmail.com>
2022-12-06 20:38:15 +0530

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,30 @@
+use ToyStore
+
+select * from Customer
+select * from Date
+
+select * from Product
+select * from ProductCategory
+select * from ProductType
+select * from Promotion
+select * from Segments
+select * from Customer
+
+select * from [Order]
+
+use Target
+delete from DateDim
+delete from StoreDim
+delete from PromotionDim
+delete from ProductDim
+delete from CustomerDim
+delete from OrderDim
+
+
+select * from PromotionDim
+select * from ProductDim
+select * from CustomerDim
+select * from OrderDim
+select * from StoreDim
+select * from DateDim
+select * from SalesFactTable
